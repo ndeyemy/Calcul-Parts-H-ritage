4e15e160d88e4c08027a1359d1d302a64a34301e
ndeyeamy fall <falamy2006@hotmail.com>
2019-04-01 22:46:34 +0000

    dernier

---
+++ TParendre/Controllers/BibliothequesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TParendre.Models;
+
+
+namespace TParendre.Controllers
+{
+    public class BibliothequesController : Controller
+    {
+        // GET: Bibliotheques
+        public ActionResult Index()
+        {
+            var bibliotheque = new Bibliotheque() { Name = "Heritage" };
+
+            return View(bibliotheque);
+        }
+    }
+}
